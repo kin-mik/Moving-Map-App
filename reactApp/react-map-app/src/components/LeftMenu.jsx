4651ee1a3ec939e12fb3b7e4a893b0534b165518
keisuke <kinta.business@gmail.com>
2023-04-04 22:41:20 +0900

    coponent left-menu

---
+++ reactApp/react-map-app/src/components/LeftMenu.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const LeftMenu = () => {
+    // const { todoText, onChange, onClick, disabled } = props;
+
+    return (
+        <div className="left-menu">
+            左メニュー
+        </div>
+    );
+};
