b413caa47244d1d1d6fabbe8cdb79623beab364c
keisuke <kinta.business@gmail.com>
2023-04-12 00:13:33 +0900

    add icon/logo

--- reactApp/react-map-app/src/components/LeftMenu.jsx
+++ reactApp/react-map-app/src/components/LeftMenu.jsx
@@ -11,27 +11,16 @@ export const LeftMenu = () => {
 
     return (
         <div className="left-menu">
-            <SearchBox />
-            <div className="search-box">
-                <p className="title">地点①</p>
-                <input placeholder="地名を検索" />
-                <button>検索</button>
-                <button>削除</button>
+            <div className="left-head">
                 <div>
-                    <label>
-                        移動手段：
-                        <select className="drop-down" value={selectedOption} onChange={handleChange}>
-                            <option value="car">車</option>
-                            <option value="bike">自転車</option>
-                            <option value="walk">徒歩</option>
-                        </select>
-                        で
-                        <input type="number" min="0" max="100" size="5"/>
-                        分以内
-                    </label>
+                    ピンの数：
+                    <input type="number" min="0" max="5" size="5" />
+                    点
                 </div>
             </div>
-
+            <SearchBox />
+            <SearchBox />
+            <SearchBox />
         </div>
     );
 };
