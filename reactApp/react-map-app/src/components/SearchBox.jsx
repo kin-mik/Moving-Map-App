368aca354749db4b6a9bd1023fdf9d2e6264f1e9
keisuke <kinta.business@gmail.com>
2023-04-26 22:26:25 +0900

    update css

--- reactApp/react-map-app/src/components/SearchBox.jsx
+++ reactApp/react-map-app/src/components/SearchBox.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 export const SearchBox = (props) => {
-    const { index ,searchValues, handleSearchBoxChange ,handlePin, disabled} = props;
+    const { index, searchValues, handleSearchBoxChange, handlePin, disabled } = props;
     const [selectedOption, setSelectedOption] = useState("");
     const handleChange = (e) => {
         setSelectedOption(e.target.value);
@@ -10,28 +10,53 @@ export const SearchBox = (props) => {
     return (
         <div className="search-box">
             <p className="title">{`地点${index + 1}`}</p>
+
             <input
-            className="search-input"
-            disabled={disabled} 
-            placeholder="地名を検索"
+                className="search-input"
+                disabled={disabled}
+                placeholder="地名を検索"
                 value={searchValues[index]}
                 onChange={handleSearchBoxChange(index)}
-                 />
-            <button disabled={disabled} onClick={()=> handlePin(index)}>検索</button>
+            />
+            <button disabled={disabled} onClick={() => handlePin(index)}>検索</button>
             <button disabled={disabled}>解除</button>
-            <div>
-                <label>
-                    移動手段：
-                    <select disabled={disabled} value={selectedOption} onChange={handleChange}>
-                        <option value="car">車</option>
-                        <option value="bike">自転車</option>
-                        <option value="walk">徒歩</option>
-                    </select>
-                    で
-                    <input className="value-input" disabled={disabled} type="number" min="0" max="100" size="5" />
-                    分以内
-                </label>
+            <div className="distance-input">
+                <input
+                    className="value-input"
+                    disabled={disabled}
+                    type="number"
+                    step="0.1"
+                    min="0"
+                    max="100" />
+                <span>km以内</span>
             </div>
         </div>
+
+
+        // <div className="search-box">
+        //     <p className="title">{`地点${index + 1}`}</p>
+        //     <input
+        //     className="search-input"
+        //     disabled={disabled} 
+        //     placeholder="地名を検索"
+        //         value={searchValues[index]}
+        //         onChange={handleSearchBoxChange(index)}
+        //          />
+        //     <button disabled={disabled} onClick={()=> handlePin(index)}>検索</button>
+        //     <button disabled={disabled}>解除</button>
+        //     <div>
+        //         <label>
+        //             移動手段：
+        //             <select disabled={disabled} value={selectedOption} onChange={handleChange}>
+        //                 <option value="car">車</option>
+        //                 <option value="bike">自転車</option>
+        //                 <option value="walk">徒歩</option>
+        //             </select>
+        //             で
+        //             <input className="value-input" disabled={disabled} type="number" min="0" max="100" size="5" />
+        //             分以内
+        //         </label>
+        //     </div>
+        // </div>
     );
 };
