a9f21ed028ee49e1bc3297192bdc794e5ac5a15b
keisuke <kinta.business@gmail.com>
2023-05-05 18:58:28 +0900

    add history table

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,31 @@ require('dotenv').config({ path: './.env' });
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "db",
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+    host: "db",
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
+
+// 新たに追加したSQLファイルを読み込む
+async function getHistory() {
+    const connection = await pool.getConnection();
+    const [rows] = await connection.execute('SELECT * FROM history');
+    connection.release();
+    return rows;
+}
+
+async function addHistory(latitude, longitude, radius) {
+    const connection = await pool.getConnection();
+    const [result] = await connection.execute(
+        'INSERT INTO history (lat, lng, radius) VALUES (?, ?, ?)',
+        [latitude, longitude, radius]
+    );
+    connection.release();
+    return result.insertId;
+}
 
 module.exports = pool.promise();
