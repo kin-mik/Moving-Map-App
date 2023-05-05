0fb7b66bc5c1e2415eb2d8b7ffbceed170aefa66
keisuke <kinta.business@gmail.com>
2023-05-05 15:56:24 +0900

    fix env file

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
+require('dotenv').config({ path: './.env' });
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: "db",
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 module.exports = pool.promise();
